4eac700687240f1e9a48c05f9da853f22532a3cb
nischal baskota <nischalbaskota08@gamil.com>
2024-03-11 13:56:20 +0545

    first commit

---
+++ src/components/employee.js
@@ -0,0 +1,31 @@
+//declaring the array 
+
+const employee =[
+    {
+
+    //creating multiple objects 
+    id: "1",
+    Name: "Nischal",
+    Age: "24",
+},
+
+    {
+
+    //creating multiple objects 
+    id: "2",
+    Name: "Nirmal",
+    Age: "24",
+},
+
+{
+
+    //creating multiple objects 
+    id: "3",
+    Name: "damodar",
+    Age: "29",
+}
+
+
+
+]
+export default employee; //it hepls to export data of employee.js
